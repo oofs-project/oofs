755bd1d073655099fc636a99c5e49b90614f2a8c
Carbon <robotcreeper13@gmail.com>
2019-11-09 19:51:16 -0600

    stuff

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import discord
 from discord import File
-import os
-from database import DBwrapper
-import splitter
+from diskcollections.iterables import FileList
 
+import splitter
+from database import DBwrapper
+
+BotChannel = 642818966825336835
 db = DBwrapper("db.json")
 
 
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    chunks = splitter.chunk('test.JPG', 1000000)
-    db.addFile('/test.JPG', [], [])
+
+async def upload(filename, VirtualPath):
+    chunks = splitter.chunk(filename, 1000000)
+    db.addFile(VirtualPath, [], [])
     i = 0
     for chunk in chunks:
         i += 1
         m = await client.guilds[0].text_channels[0].send(file=File(chunk, filename=str(i)))
-        db.addChunk('/test.JPG', m.id)
+        db.addChunk(VirtualPath, m.id)
+
+@client.event
+async def on_ready():
+    flist = FileList()
+    Ids = db.getChunks("/test.JPG")  # List of message IDs
+    for id in Ids:
+        message = client.get_channel(BotChannel).get_message(id)
+        print(message)
+
 
 client.run(open(".env", "r").read())
--- splitter/__init__.py
+++ splitter/__init__.py
@@ -1,19 +1,20 @@
-import encdec
+import io
+from diskcollections.iterables import FileList
 import io
 
+from diskcollections.iterables import FileList
+
+
 def chunk(filename, chunksize):
-    chunks = []
+    chunks = FileList()
     file_number = 1
     with open(filename, "rb") as f:
         chunk = f.read(chunksize)
         while chunk:
-            with open("chunks/" + filename[:-4] + str(file_number), "wb") as chunk_file:
-                # encodedfile = encdec.encodeBits(chunk)
-                chunk_file.write(chunk)
-                chunks.append(io.BytesIO(chunk))
+            chunks.append(io.BytesIO(chunk))
             file_number += 1
             chunk = f.read(chunksize)
-        return chunks
+    return chunks
 
 def unchunk(chunks):
     pass
