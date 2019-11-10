e2b2d37e97bf3ab35438d592ffd9ccfae1f2cb3a
Carbon <robotcreeper13@gmail.com>
2019-11-10 05:37:51 -0600

    Big stuff

--- newmain.py
+++ newmain.py
@@ -4,13 +4,14 @@ BotChannel = 642818966825336835
 
 DFS = DiscordFileSystem('db.json')
 
-file = DFS.openbin("/jetbrains.exe", mode="wb")
-with open('jetbrains-toolbox-1.11.4269.exe', 'rb') as f:
+file = DFS.openbin("/1g.bin", mode="wb")
+with open('1GB.bin', 'rb') as f:
     file.write(f.raw.read())
 
 file.copy()
 file.close()
 print("wrote")
 
-with open('jetbrains.exe', 'wb') as f:
-    f.write(DFS.openbin('/jetbrains.exe').getvalue())
+with open('1GB discorded.bin', 'wb') as f:
+    f.write(DFS.openbin('/1g.bin').getvalue())
+print("finish")
